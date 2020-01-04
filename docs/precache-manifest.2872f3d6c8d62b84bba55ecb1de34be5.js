6cd01ef8ce159bf2648f095f2ec4246f922ec526
Jenny Ngai <jennyshngai+foo@gmail.com>
2020-01-04 13:32:37 +0100

    point build to docs

---
+++ docs/precache-manifest.2872f3d6c8d62b84bba55ecb1de34be5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be12dd6fbbb4e6c513256b5857e4a7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "300e805fde380d23bec8",
+    "url": "/static/css/main.1212f680.chunk.css"
+  },
+  {
+    "revision": "e9f380fa68422b823645",
+    "url": "/static/js/2.71f4e20b.chunk.js"
+  },
+  {
+    "revision": "df26b846936047f03ec17db12af485f1",
+    "url": "/static/js/2.71f4e20b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "300e805fde380d23bec8",
+    "url": "/static/js/main.b4d02028.chunk.js"
+  },
+  {
+    "revision": "6f19745d8a716abb4ac7",
+    "url": "/static/js/runtime-main.ec26b0bf.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);
